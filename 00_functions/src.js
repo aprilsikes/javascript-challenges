2f655ff1696a1f6f15c5f63467490b9e779eb97c
april sikes <aprilsikes@gmail.com>
2015-12-03 21:09:01 -0700

    js challenges 00_functions

--- 00_functions/src.js
+++ 00_functions/src.js
@@ -1,12 +1,39 @@
 module.exports = {
 
   helloWorld: function(){
+    return "Hello World";
   },
 
   hello: function(string){
+    return "Hello " + string;
   },
 
-  shout: function() {
+  shout: function(string) {
+    return "Hello " + string.toUpperCase();
+  },
 
+  whisper: function(string) {
+    return "Hello " + string.toLowerCase();
+  },
+
+  separate: function(string) {
+    return string.split("");
+  },
+
+  reverseJoin: function(array) {
+    var newArray = array.reverse();
+    return newArray.join(",");
+  },
+
+  reverseJoinCompact: function(array) {
+    var newArray = array.reverse();
+    return newArray.join("");
+  },
+
+  backwardsDay: function(string) {
+    var array = string.split("");
+    var newArray = array.reverse();
+    var newString = newArray.join("")
+    return "Hello " + newString;
   }
 }
