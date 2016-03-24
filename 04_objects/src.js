8103c8dd89681c1610f276dae622376a2376a69d
april sikes <aprilsikes@gmail.com>
2016-03-24 17:17:57 -0600

    04 passing

--- 04_objects/src.js
+++ 04_objects/src.js
@@ -1,5 +1,32 @@
 module.exports = {
   person: function() {
-    // your code here
+    var person = {
+      firstName: "John",
+      lastName: "Mayer"
+    }
+    return person;
+  },
+  sweetnessProperty: function (obj) {
+    if (!obj) {
+      return undefined;
+    } else {
+      return obj['sweetness'];
+    }
+  },
+  savorynessProperty: function (obj) {
+    if  (!obj) {
+      return undefined;
+    } else {
+      return obj['savory-ness'];
+    }
+  },
+  keys: function (obj) {
+    return Object.keys(obj);
+  },
+  sortedKeys: function (obj) {
+    return Object.keys(obj).sort();
+  },
+  reverseSortedKeys: function (obj) {
+    return Object.keys(obj).sort().reverse();
   }
 }
