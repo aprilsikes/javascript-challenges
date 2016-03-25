b171fc4747f7534046c32e29ee6dc8742893a556
april sikes <aprilsikes@gmail.com>
2016-03-24 18:32:05 -0600

    06 passing

--- 06_join_for_loop/src.js
+++ 06_join_for_loop/src.js
@@ -5,7 +5,35 @@
 module.exports = {
 
   joinWithFor: function(list) {
-    // your code here
+    return list.join("");
+  },
+  joinWithForAndIndex: function (list) {
+    var join = [];
+    for (var i = 0; i < list.length; i++) {
+      join.push(list[i], i);
+    } return join.join("");
+  },
+  joinWithoutOddCharacters: function (list) {
+    var even = [];
+    for (var i = 0; i < list.length; i++) {
+      if (i % 2 === 0) {
+        even.push(list[i]);
+      }
+    } return even.join("");
+  },
+  joinWithForAndToken: function (list, token) {
+    return list.join(token);
+  },
+  joinWithForAndAlternatingTokens: function (list, token1, token2) {
+    var join = [];
+    for (var i = 0; i < list.length; i++) {
+      if (i % 2 === 0) {
+        join.push(list[i], token1);
+      } else {
+        join.push(list[i], token2);
+      }
+    } join.pop();
+      return join.join("");
   }
 
 }
